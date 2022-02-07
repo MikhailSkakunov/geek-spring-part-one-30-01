fd3b81cff4fb48dd096ef601c02a35c961b90d06
Mikhail <skakunovmy@gmail.com>
2022-02-07 13:30:09 +0300

    commit

---
+++ lesson-07-spring-boot-hib/src/test/java/ru/geekbrains/demolesson07springboothib/Demolesson07SpringBootHibApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.demolesson07springboothib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+//class Demolesson07SpringBootHibApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
